e1bc15487c2afc9483298b4f005432d66554f6fd
NikhilRout <nikhilrout97@gmail.com>
2025-02-05 02:47:47 +0530

    added USBA

---
+++ Carry Look Ahead Adder/CLA_nbit/CLA_nbit.v
@@ -0,0 +1,31 @@
+module CLA_nbit #(parameter N = 4)(
+    input [N-1:0] A, B,
+    output [N:0] Sum
+);
+    wire [N:0] carry;
+    wire [N-1:0] Gen, Prop, w_sum;
+
+    genvar i;
+    generate
+        for(i=0; i<N; i=i+1)
+            FullAdder FA(.a(A[i]), .b(B[i]), .cin(carry[i]), .sum(w_sum[i]));
+    endgenerate
+    
+    assign carry[0] = 1'b0;
+    genvar j;
+    generate
+        for(j=0; j<N; j=j+1) begin
+            assign Gen[j] = A[j] & B[j];
+            assign Prop[j] = A[j] ^ B[j];
+            assign carry[j+1] = Gen[j] | (Prop[j] & carry[j]);
+        end
+    endgenerate
+    assign Sum = {carry[N], w_sum};
+endmodule
+
+module FullAdder(
+    input a, b, cin, 
+    output sum
+);
+    assign sum = a ^ b ^ cin;
+endmodule
